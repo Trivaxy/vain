5bf3261a67be72467f0d12a25d45ad65179295ea
Trivaxy <itsmeomar2003@yahoo.com>
2020-07-25 15:05:39 +0300

    Add token IDs + stop substringing

--- vain.v
+++ vain.v
@@ -10,46 +10,47 @@ mut:
 	input        string
 }
 
-pub fn (mut lexer Lexer) next() ?string {
+pub fn (mut lexer Lexer) next() ?(string, string) {
 	if lexer.pos == lexer.input.len {
 		return none
 	}
 
 	for rule in lexer.rules {
 		if rule.regex {
-			mut regex, err, pos := regex.regex(rule.str_rule)
+			mut regex, err, err_pos := regex.regex(rule.str_rule)
 
 			if err != 0 {
-				return error_with_code("invalid regex ${rule.str_rule} at position $pos", err)
+				return error_with_code("invalid regex ${rule.str_rule} at position $err_pos", err)
 			}
 
-			start, end := regex.match_string(lexer.input)
-		
+			start, end := regex.match_string(lexer.input[lexer.pos..])
+
 			if start != 0 {
 				continue
 			}
 
-			token := rule.callback(lexer.input.substr(start, end))
-			lexer.input = lexer.input.substr(end, lexer.input.len)
-			return token
+			token := rule.callback(lexer.input[lexer.pos + start..lexer.pos + end])
+			lexer.pos += end
+			return rule.id, token
 		}
 		else {
-			read := lexer.input.substr(0, rule.str_rule.len)
+			read := lexer.input[lexer.pos..lexer.pos + rule.str_rule.len]
 
 			if rule.str_rule != read {
 				continue
 			}
 
 			token := rule.callback(read)
-			lexer.input = lexer.input.substr(read.len, lexer.input.len)
-			return token
+			lexer.pos += rule.str_rule.len
+			return rule.id, token
 		}
 	}
 
-	lexer.err_callback(lexer.input)
+	lexer.err_callback(lexer.input[lexer.pos..])
 }
 
 struct LexRule {
+	id       string
 	str_rule string
 	callback fn (string) string
 	regex    bool
@@ -59,32 +60,36 @@ fn do_nothing (str string) string {
 	return str
 }
 
-pub fn literal(str string) LexRule {
+pub fn literal(tok_id string, str string) LexRule {
 	return LexRule{
+		id: tok_id,
 		str_rule: str,
 		callback: do_nothing,
 		regex: false
 	}
 }
 
-pub fn literal_callback(str string, cb fn (string) string) LexRule {
+pub fn literal_callback(tok_id string, str string, cb fn (string) string) LexRule {
 	return LexRule{
+		id: tok_id,
 		str_rule: str,
 		callback: cb,
 		regex: false
 	}
 }
 
-pub fn regex(str string) LexRule {
+pub fn regex(tok_id string, str string) LexRule {
 	return LexRule {
+		id: tok_id,
 		str_rule: str,
 		callback: do_nothing,
 		regex: true
 	}
 }
 
-pub fn regex_callback(str string, cb fn (string) string) LexRule {
+pub fn regex_callback(tok_id string, str string, cb fn (string) string) LexRule {
 	return LexRule{
+		id: tok_id,
 		str_rule: str,
 		callback: cb,
 		regex: true
